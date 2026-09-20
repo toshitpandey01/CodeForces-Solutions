811e32c6943c41794274501bb3b68814c2446b06
Toshit Pandey <toshitpandey01@gmail.com>
2026-09-20 06:52:26 +0530

    1669A Solution

---
+++ 1669A(Division).py
@@ -0,0 +1,11 @@
+t=int(input())
+for i in range(t):
+    rating=int(input())
+    if 1900<=rating:
+        print("Division 1")
+    elif 1600<=rating<=1899:
+        print("Division 2")
+    elif 1400<=rating<=1599:
+        print("Division 3")
+    else:
+        print("Division 4")
